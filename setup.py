ae6f769e6c718b67eab66125894e221b39c95644
qqfunc <qqfunc@gmail.com>
2025-02-16 21:42:42 +0900

    Create `setup.py`

---
+++ setup.py
@@ -0,0 +1,24 @@
+"""setup.py: Set configurations of setuptools."""
+
+from pathlib import Path
+from subprocess import check_output
+
+from setuptools import Extension, setup
+
+C_EXT_PATH = Path("csrc")
+BREW_PREFIX = check_output(
+    ["/opt/homebrew/bin/brew", "--prefix"],
+    text=True,
+).removesuffix("\n")
+
+setup(
+    ext_modules=[
+        Extension(
+            name="objctypes",
+            sources=C_EXT_PATH.glob("*.c", "*.cpp"),
+            include_dirs=[f"{BREW_PREFIX}/include"],
+            library_dirs=[f"{BREW_PREFIX}/lib"],
+            libraries=["ffi"],
+        ),
+    ],
+)
