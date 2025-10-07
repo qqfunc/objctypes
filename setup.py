9512a3088f553dfa9dc6a3d7772927e0c68e971c
qqfunc <qqfunc@gmail.com>
2025-10-07 19:46:06 +0900

    chore: fix LibFFI prefix path

--- setup.py
+++ setup.py
@@ -10,9 +10,9 @@ from setuptools import Extension, setup
 if sys.platform == "darwin":
     C_EXT_PATH = Path("csrc")
     BREW_PATH = Path("/opt/homebrew/bin/brew")
-    BREW_PREFIX = (
+    LIBFFI_PREFIX = (
         check_output(
-            ["/opt/homebrew/bin/brew", "--prefix"],
+            ["/opt/homebrew/bin/brew", "--prefix", "libffi"],
             text=True,
         ).removesuffix("\n")
         if Path("/opt/homebrew/bin/brew").is_file()
@@ -27,12 +27,12 @@ if sys.platform == "darwin":
                     C_EXT_PATH.glob("*.cpp"),
                 ),
                 include_dirs=None
-                if BREW_PREFIX is None
-                else [f"{BREW_PREFIX}/include"],
+                if LIBFFI_PREFIX is None
+                else [f"{LIBFFI_PREFIX}/include"],
                 library_dirs=None
-                if BREW_PREFIX is None
-                else [f"{BREW_PREFIX}/lib"],
-                libraries=None if BREW_PREFIX is None else ["ffi"],
+                if LIBFFI_PREFIX is None
+                else [f"{LIBFFI_PREFIX}/lib"],
+                libraries=None if LIBFFI_PREFIX is None else ["ffi"],
             ),
         ],
     )
