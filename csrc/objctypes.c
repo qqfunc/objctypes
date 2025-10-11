adcc25b6a9f9cc23a0adc4f825dee618528bb8e9
qqfunc <qqfunc@gmail.com>
2025-10-11 19:50:01 +0900

    feat: introduce multi-phase initialization (#231)

--- csrc/objctypes.c
+++ csrc/objctypes.c
@@ -2,64 +2,67 @@
 
 #include "objctypes.h"
 
-static PyModuleDef objctypesmodule = {
+static int
+objctypes_module_exec(PyObject *m)
+{
+    // Add ObjCClass
+    ObjCClassType.tp_base = &PyType_Type;
+    if (PyModule_AddType(m, &ObjCClassType) < 0) {
+        return -1;
+    }
+
+    // Add ObjCObject
+    Py_SET_TYPE(&ObjCObjectType, &ObjCClassType);
+    if (PyModule_AddType(m, (PyTypeObject *)&ObjCObjectType) < 0) {
+        return -1;
+    }
+
+    // Add ObjCMethod
+    if (PyModule_AddType(m, &ObjCMethodType) < 0) {
+        return -1;
+    }
+
+    // Add ObjCSelector
+    if (PyModule_AddType(m, &ObjCSelectorType) < 0) {
+        return -1;
+    }
+
+    // Add ObjCBool
+    if (PyModule_AddType(m, &ObjCBoolType) < 0) {
+        return -1;
+    }
+
+    // Add YES
+    PyObject *objc_yes = ObjCBool_FromLong(1);
+    if (PyModule_Add(m, "YES", objc_yes) < 0) {
+        return -1;
+    }
+
+    // Add NO
+    PyObject *objc_no = ObjCBool_FromLong(0);
+    if (PyModule_Add(m, "NO", objc_no) < 0) {
+        return -1;
+    }
+
+    return 0;
+}
+
+static PyModuleDef_Slot objctypes_module_slots[] = {
+    {Py_mod_exec, objctypes_module_exec},
+    {Py_mod_multiple_interpreters, Py_MOD_MULTIPLE_INTERPRETERS_NOT_SUPPORTED},
+    {0, NULL}
+};
+
+static PyModuleDef objctypes_module = {
     .m_base = PyModuleDef_HEAD_INIT,
     .m_name = "objctypes",
     .m_doc = "A bridge between Objective-C and Python.",
-    .m_size = -1,
+    .m_size = 0,
+    .m_slots = objctypes_module_slots,
 };
 
 PyMODINIT_FUNC
 PyInit_objctypes(void)
 {
-    PyObject *module = PyModule_Create(&objctypesmodule);
-    if (module == NULL) {
-        return NULL;
-    }
-
-    // Add ObjCClass
-    ObjCClassType.tp_base = &PyType_Type;
-    if (PyModule_AddType(module, &ObjCClassType) < 0) {
-        return NULL;
-    }
-    Py_XDECREF(&ObjCClassType);
-
-    // Add ObjCObject
-    Py_SET_TYPE(&ObjCObjectType, &ObjCClassType);
-    if (PyModule_AddType(module, (PyTypeObject *)&ObjCObjectType) < 0) {
-        return NULL;
-    }
-    Py_XDECREF(&ObjCObjectType);
-
-    // Add ObjCMethod
-    if (PyModule_AddType(module, &ObjCMethodType) < 0) {
-        return NULL;
-    }
-    Py_XDECREF(&ObjCMethodType);
-
-    // Add ObjCSelector
-    if (PyModule_AddType(module, &ObjCSelectorType) < 0) {
-        return NULL;
-    }
-    Py_XDECREF(&ObjCSelectorType);
-
-    // Add ObjCBool
-    if (PyModule_AddType(module, &ObjCBoolType) < 0) {
-        return NULL;
-    }
-    Py_XDECREF(&ObjCBoolType);
-
-    // Add YES
-    PyObject *objc_yes = ObjCBool_FromLong(1);
-    if (PyModule_Add(module, "YES", objc_yes) < 0) {
-        return NULL;
-    }
-
-    // Add NO
-    PyObject *objc_no = ObjCBool_FromLong(0);
-    if (PyModule_Add(module, "NO", objc_no) < 0) {
-        return NULL;
-    }
-
-    return module;
+    return PyModuleDef_Init(&objctypes_module);
 }
