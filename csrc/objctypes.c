1cbdf028b7f209be01a44cbfd4b31981e186aaa9
qqfunc <qqfunc@gmail.com>
2025-10-15 19:55:50 +0900

    chore: introduce module state

--- csrc/objctypes.c
+++ csrc/objctypes.c
@@ -2,49 +2,74 @@
 
 #include "objctypes.h"
 
+#include "objctypes_module.h"
+
 static int
-objctypes_module_exec(PyObject *m)
+objctypes_module_exec(PyObject *module)
 {
+    // Get the module state
+
     // Add ObjCClass
     ObjCClassType.tp_base = &PyType_Type;
-    if (PyModule_AddType(m, &ObjCClassType) < 0) {
+    if (PyModule_AddType(module, &ObjCClassType) < 0) {
         return -1;
     }
 
     // Add ObjCObject
     Py_SET_TYPE(&ObjCObjectType, &ObjCClassType);
-    if (PyModule_AddType(m, (PyTypeObject *)&ObjCObjectType) < 0) {
+    if (PyModule_AddType(module, (PyTypeObject *)&ObjCObjectType) < 0) {
         return -1;
     }
 
     // Add ObjCMethod
-    if (PyModule_AddType(m, &ObjCMethodType) < 0) {
+    if (PyModule_AddType(module, &ObjCMethodType) < 0) {
         return -1;
     }
 
     // Add ObjCSelector
-    if (PyModule_AddType(m, &ObjCSelectorType) < 0) {
+    if (PyModule_AddType(module, &ObjCSelectorType) < 0) {
         return -1;
     }
 
     // Add ObjCBool
-    if (PyModule_AddType(m, &ObjCBoolType) < 0) {
+    if (PyModule_AddType(module, &ObjCBoolType) < 0) {
         return -1;
     }
 
     // Add YES
     PyObject *objc_yes = ObjCBool_FromLong(1);
-    if (PyModule_Add(m, "YES", objc_yes) < 0) {
+    if (PyModule_Add(module, "YES", objc_yes) < 0) {
         return -1;
     }
 
     // Add NO
     PyObject *objc_no = ObjCBool_FromLong(0);
-    if (PyModule_Add(m, "NO", objc_no) < 0) {
+    if (PyModule_Add(module, "NO", objc_no) < 0) {
         return -1;
     }
 
     return 0;
+}
+
+static int
+objctypes_module_traverse(PyObject *Py_UNUSED(module),
+                          visitproc Py_UNUSED(visit), void *Py_UNUSED(arg))
+{
+    // objctypes_state *state = PyModule_GetState(module);
+    return 0;
+}
+
+static int
+objctypes_module_clear(PyObject *Py_UNUSED(module))
+{
+    // objctypes_state *state = PyModule_GetState(module);
+    return 0;
+}
+
+static void
+objctypes_module_free(void *module)
+{
+    objctypes_module.m_clear(module);
 }
 
 static PyModuleDef_Slot objctypes_module_slots[] = {
@@ -53,12 +78,15 @@ static PyModuleDef_Slot objctypes_module_slots[] = {
     {0, NULL},
 };
 
-static PyModuleDef objctypes_module = {
+PyModuleDef objctypes_module = {
     .m_base = PyModuleDef_HEAD_INIT,
     .m_name = "objctypes",
     .m_doc = "A bridge between Objective-C and Python.",
-    .m_size = 0,
+    .m_size = sizeof(objctypes_state),
     .m_slots = objctypes_module_slots,
+    .m_traverse = objctypes_module_traverse,
+    .m_clear = objctypes_module_clear,
+    .m_free = (freefunc)objctypes_module_free,
 };
 
 PyMODINIT_FUNC
---
+++ csrc/objctypes_module.h
@@ -0,0 +1,11 @@
+#ifndef OBJCTYPES_MODULE_H
+#define OBJCTYPES_MODULE_H
+
+#include <Python.h>
+
+extern PyModuleDef objctypes_module;
+
+typedef struct {
+} objctypes_state;
+
+#endif // OBJCTYPES_MODULE_H
